dd9a5c36aaf5aa15f03406638a239baa5a9fdf68
paytonthepigeon <paytonschrama@gmail.com>
2024-09-12 22:26:07 +1200

    Final Product
    I am very proud of what I have achieved with this project I wish I could have done more detail in the background but overall I still love the end product.

--- music_starter.js
+++ music_starter.js
@@ -16,7 +16,7 @@ fill(80, 40, 49)
 rect(350,250,400,150)
 fill(80, 40, 49)
 rect(350,450,400,125)
-   //ellipse(10+Xmove,100,45,45); 
+
    //potionglow
    beginShape()
 fill(44, 150, 89, 90)
@@ -77,7 +77,7 @@ vertex(900,510)
 vertex(700,530)
 vertex(680,450)
 vertex(600,400)
-//vertex(800,400)
+
 
 endShape()
 //face
@@ -137,11 +137,7 @@ stroke(193,112,96)
 
 
 
-//  fill(69)
-//  for(let i =0;i < 6;i++)
-//  {
-//   ellipse(50+100*i,70,70)
-//  }
+
 //coat base
    beginShape()
    stroke(20,30,60)
@@ -217,43 +213,3 @@ text(words, width / 20, height / 5); // display current lyric in middle of page,
 }
 
 
-
-//    textFont('Verdana'); // please use CSS safe fonts
-//  rectMode(CENTER)
-//    textSize(24);
-  
-//     let bar_spacing = height / 10;
-//     let bar_height = width / 12;
-//     let bar_pos_x = width / 2;
- 
-
-//     // vocal bar is red
-//     fill(200, 0, 0);
-//     rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-//     fill(0);
-//     text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-//     // drum bar is green
-  
-//   fill(0, 200, 0);
-//     rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-//     fill(0);
-//     text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-//     // bass bar is blue
-//     fill(50, 50, 240);
-//     rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-//     fill(0);
-//     text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-//  //other bar is white
-//     fill(200, 200, 200);
-//     rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-//     fill(0);
-//     text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-//     fill(255, 255, 0);
- 
-//     // display "words"
-//     textAlign(CENTER);
-//     textSize(vocal);
-//    text(words, width/8, height/10);
